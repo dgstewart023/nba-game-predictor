06b02eaf9dd4af74c2167983b6e021bbedaed441
dgstewart023 <dgstewart023@gmail.com>
2025-09-30 16:46:34 -0400

    getting relavant stats

--- game_predictorV1.py
+++ game_predictorV1.py
@@ -1,4 +1,4 @@
-from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguedashteamstats
 import csv
 
 def get_team_id(identifier):
@@ -28,3 +28,26 @@ def get_team_id(identifier):
                 return team_id
             
         return None
+    
+
+def get_single_team_stats(team_id):
+    """Get efficiency stats for one team"""
+    stats = leaguedashteamstats.LeagueDashTeamStats(
+        season='2024-25',
+        measure_type_detailed_defense='Four Factors',
+        per_mode_detailed='PerGame'
+    )
+    df = stats.get_data_frames()[0]
+    
+    team_data = df[df['TEAM_ID'] == int(team_id)]
+
+    four_factors_data = team_data[['TEAM_NAME',
+                            'EFG_PCT',
+                            'TM_TOV_PCT',
+                            'OREB_PCT',
+                            'FTA_RATE',
+                            'OPP_EFG_PCT',
+                            'OPP_TOV_PCT',
+                            'OPP_OREB_PCT',
+                            'OPP_FTA_RATE']]
+    return four_factors_data
